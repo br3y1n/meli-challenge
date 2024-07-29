2425fa7ef4214afdb4f2df773d87a1fc8cc9892f
Brayan Arango <br3y1n@hotmail.es>
2024-07-29 10:41:46 -0500

    fix: remove parasitic files

--- packages/server/src/shared/domain/Pagination/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./PaginationEntity";
-export * from "./PaginationLimitVO";
-export * from "./PaginationMaxItemsVO";
-export * from "./PaginationOffsetVO";
-export * from "./PaginationTotalVO";
